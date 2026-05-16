e4ac6d8b85bcc9a5e6ccb7672b3f3b729b432633
salahuddingfx <salauddinkaderappy@gmail.com>
2026-05-16 21:27:07 +0600

    feat: create Project mongoose schema for portfolio backend

--- models/Project.js
+++ models/Project.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 const projectSchema = new mongoose.Schema({
   title: { type: String, required: true },
@@ -12,4 +12,5 @@ const projectSchema = new mongoose.Schema({
   order: { type: Number, default: 0 }
 }, { timestamps: true });
 
-module.exports = mongoose.model('Project', projectSchema);
+const Project = mongoose.model('Project', projectSchema);
+export default Project;
